4a3cf8ff2daca7988fb325a8628e24dff89cbafa
niklasfriesen <niklasfriesen01@pm.me>
2026-08-20 14:11:43 +0200

    Changed design

--- src/algorithms/bubble_sort.py
+++ src/algorithms/bubble_sort.py
@@ -1,12 +1,10 @@
-
 def bubble_sort(arr: list) -> list:
-    # Sort a list using bubble sort
-    # Returns a new sorted list, does not change the original list
 
     result = arr.copy()
     n = len(result)
 
     for i in range(n):
+
         is_sorted = True
 
         for k in range(n - i - 1, ): # i elements are already sorted
